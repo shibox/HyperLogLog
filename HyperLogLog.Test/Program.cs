d59c99d2ceef9b5d2468a41380a8e25d0923918f
shibox <jarvey.create@163.com>
2016-05-11 19:21:55 +0800

    修改测试代码

--- HyperLogLog.Test/Program.cs
+++ HyperLogLog.Test/Program.cs
@@ -11,20 +11,39 @@ namespace HyperLogLog.Test
     {
         static void Main(string[] args)
         {
+            TestCountInt32();
+            //TestCountString();
+        }
+
+        private static void TestCountInt32()
+        {
             IHyperLogLog<int> estimator = new CardinalityEstimator(14);
 
             Stopwatch w = Stopwatch.StartNew();
             for (int i = 1; i <= 1000000; i++)
                 estimator.Add(i);
-            //estimator.Add("Alice");
-            //estimator.Add("Bob");
-            //estimator.Add("Alice");
-            //estimator.Add("George Michael");
             w.Stop();
 
-            ulong numberOfuniqueElements = estimator.Count(); // will be 3
-            Console.WriteLine(numberOfuniqueElements + "    cost:" + w.ElapsedMilliseconds);
+            ulong count = estimator.Count();
+            Console.WriteLine(count + "    cost:" + w.ElapsedMilliseconds);
             Console.ReadLine();
         }
+
+        private static void TestCountString()
+        {
+            IHyperLogLog<string> estimator = new CardinalityEstimator(14);
+            Stopwatch w = Stopwatch.StartNew();
+            estimator.Add("Alice");
+            estimator.Add("Bob");
+            estimator.Add("Alice");
+            estimator.Add("George Michael");
+            w.Stop();
+
+            ulong count = estimator.Count(); // will be 3
+            Console.WriteLine(count + "    cost:" + w.ElapsedMilliseconds);
+            Console.ReadLine();
+            
+        }
+
     }
 }
